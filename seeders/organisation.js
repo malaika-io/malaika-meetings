c46501cb3c0755594948476b480979021ebe9aae
karimmhamdi <contact@nourlabs.fr>
2020-04-16 21:37:41 +0200

    correction

--- seeders/organisation.js
+++ seeders/organisation.js
@@ -5,13 +5,13 @@ const moment = require("moment");
 module.exports = {
     up: (queryInterface, Sequelize) => {
 
-     /*   return queryInterface.bulkInsert('Organization', [{
+        return queryInterface.bulkInsert('Organization', [{
             id: 1,
             name: "libheros",
             created_at: moment(new Date()).format("YYYY-MM-DD HH:mm:ss"),
             updated_at: moment(new Date()).format("YYYY-MM-DD HH:mm:ss")
         }], {});
-*/
+
     },
 
     down: (queryInterface, Sequelize) => {
